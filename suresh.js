b9db023419347675ce3fa3ce5626104a6b0998e3
sureshnanjan <sureshnanjan@outlook.com>
2025-02-04 12:58:27 +0530

    Day1 Code

--- suresh.js
+++ suresh.js
@@ -1,10 +1,54 @@
 // Dashboard
+/*
+lfdkaslkf;lsf
+asdf
+dasfad
+fdsaf
+
+dsfa
+asd
+asdf
+fa
+**/
 // Title: PLayweright Training
 // Date Start:
 // Date End:
 // Participants:
 // Trainer Name:
 
+let a = null // null undefined
+let age = 40; // number
+let score = 10.01; // number
+let myname = "suresh" // string
+let singlename = 'a';
+console.log(typeof myname);
+console.log(typeof singlename)
+let employee = {name: 'suresh', occupation: 'training'}
+console.log(typeof age);
+let participants = ['suresh', 'test']
+let onoff = false;
+let dosomething = function(){ console.log("I am a function")}
+console.log(typeof dosomething);
+console.log(typeof participants);
+console.log(typeof onoff)
+//myname.
+dosomething();
+//score();
+
+let heroku_homepage = {heading:'a', subtitle:'', abtesting}
+let abtest_testing = {}
+/*
+
+typeof "John"         // Returns string
+typeof ("John"+"Doe") // Returns string
+typeof 3.14           // Returns number
+typeof 33             // Returns number
+typeof (33 + 66)      // Returns number
+typeof true           // Returns boolean
+typeof false          // Returns boolean
+typeof 1234n          // Returns bigint
+typeof Symbol()       // Returns symbol
+typeof x              // Returns undefined
 console.log("Playwright Automation")
 console.log("Start Date")
 console.log(Date.now().toString(2))
@@ -21,3 +65,4 @@ title = 100;
 console.log(title.toUpperCase());
 //console.log(participantcount.toUpperCase())
 //fixedtitle = "Something else for fixed";
+**/
