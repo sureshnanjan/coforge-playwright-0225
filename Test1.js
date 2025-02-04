f010971f9b792f94b7a623534d59811d5e8d5051
Sunil-siddharth1904 <126785144+Sunil-siddharth1904@users.noreply.github.com>
2025-02-04 12:59:14 +0530

    First commit of Test1.js

---
+++ Test1.js
@@ -0,0 +1,29 @@
+let name = "Sunil"
+let id = 107064;
+
+var surname = "Siddharth";
+var DOB = 1904;
+
+let emp = {name: 'Sunil', id: 107064 }
+
+let arr = [1, 'Hi'];
+
+let boo = false;
+
+let fn = function(){
+    console.log("Inside fn");
+}
+
+console.log("Name is "+ name); 
+console.log("Id is "+ id);
+console.log("surname is "+ surname);
+console.log("DOB is "+ DOB);
+console.log("arr is "+ DOB);
+
+console.log(typeof name);
+console.log(typeof id);
+console.log(typeof emp.name);
+console.log(typeof arr.at(1));
+console.log(typeof boo);
+
+fn();
